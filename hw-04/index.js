fe1204aea7055ced991b31696b0a18554c70e67b
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2024-10-01 10:07:47 +0300

    finished 4.2

--- hw-04/index.js
+++ hw-04/index.js
@@ -2,3 +2,20 @@
 
 const userName = prompt("What is your name?");
 alert(`Hello ${userName}! How are you?`);
+
+// HW 4.2
+
+const enteredNumber = prompt("Enter three-digit number"); //string
+const digits = enteredNumber.split(""); //array of strings
+
+const allEqual = digits[0] === digits[1] && digits[1] === digits[2];
+const someEqual =
+  digits[0] === digits[1] || digits[0] === digits[2] || digits[1] === digits[2];
+
+if (allEqual) {
+  console.log("All digits are equal");
+} else if (someEqual) {
+  console.log("Some digits are equal");
+} else {
+  console.log("All digits are not equal");
+}
