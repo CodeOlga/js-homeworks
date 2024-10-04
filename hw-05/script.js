4af3738e0ed189dbe85c603bfe5fc1009dcd7d15
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2024-10-04 08:23:01 +0300

    finished 5.1

---
+++ hw-05/script.js
@@ -0,0 +1,15 @@
+// HW 5.1
+
+let number = 20;
+let result = "";
+
+while (number <= 30) {
+  result += `${number} `;
+  number += 0.5;
+}
+
+console.log(result.trimEnd());
+
+// HW 5.2
+// HW 5.3
+// HW 5.4
