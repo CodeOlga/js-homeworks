bc6a7071e10c9e889853b5da65e7c5a7e4dc7f4c
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2024-10-11 15:08:52 +0300

    finished 6.2

--- hw-06/script.js
+++ hw-06/script.js
@@ -1,42 +1,42 @@
-// // HW 6.1 - 1 спосіб
+// HW 6.1 - 1 спосіб
 
-// const enteredString = prompt("Введіть будь-який рядок");
-// const enteredSymbols = prompt("Введіть символи, які б хотіли видалити").split(
-//   ""
-// ); //string to array of strings
+const enteredString = prompt("Введіть будь-який рядок");
+const enteredSymbols = prompt("Введіть символи, які б хотіли видалити").split(
+  ""
+); //string to array of strings
 
-// function removeSymbols(stringToChange, symbolsToRemove) {
-//   let result = "";
+function removeSymbols(stringToChange, symbolsToRemove) {
+  let result = "";
 
-//   for (const currentSymbol of stringToChange) {
-//     if (!symbolsToRemove.includes(currentSymbol)) {
-//       result += currentSymbol;
-//     }
-//   }
+  for (const currentSymbol of stringToChange) {
+    if (!symbolsToRemove.includes(currentSymbol)) {
+      result += currentSymbol;
+    }
+  }
 
-//   return result;
-// }
+  return result;
+}
 
-// console.log(removeSymbols(enteredString, enteredSymbols));
+console.log(removeSymbols(enteredString, enteredSymbols));
 
-// // HW 6.1 - 2 спосіб
+// HW 6.1 - 2 спосіб
 
-// const enteredStringNew = prompt("Введіть будь-який рядок");
-// const enteredSymbolsNew = prompt(
-//   "Введіть символи, які б хотіли видалити"
-// ).split("");
+const enteredStringNew = prompt("Введіть будь-який рядок");
+const enteredSymbolsNew = prompt(
+  "Введіть символи, які б хотіли видалити"
+).split("");
 
-// function removeSymbolsNew(stringToChangeNew, symbolsToRemoveNew) {
-//   const regex = new RegExp(`[${symbolsToRemoveNew}.join('')]`, "g");
-//   return stringToChangeNew.replace(regex, "");
-// }
+function removeSymbolsNew(stringToChangeNew, symbolsToRemoveNew) {
+  const regex = new RegExp(`[${symbolsToRemoveNew}.join('')]`, "g");
+  return stringToChangeNew.replace(regex, "");
+}
 
-// const resultNew = removeSymbolsNew(enteredStringNew, enteredSymbolsNew);
-// console.log(resultNew);
+const resultNew = removeSymbolsNew(enteredStringNew, enteredSymbolsNew);
+console.log(resultNew);
 
-// // HW 6.2
+// HW 6.2
 
-const arrayOfElements = [1, "a", true, 2, 3, undefined, 4, 5];
+const arrayOfElements = [1, "a", true, 2, 3, undefined, 4, 5, 6];
 
 function calcAverageOfNumbers(array) {
   let sumOfNumbers = 0;
