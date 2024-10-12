e10ab1f708aba36f5e0903f94cf7f905e23b5102
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2024-10-12 16:03:50 +0300

    finished 7.1, 7.2

---
+++ hw-07/script.js
@@ -0,0 +1,29 @@
+// HW 7.1
+
+function getSum() {
+  let y = 0;
+
+  return function (x) {
+    y += x; // y = y + x;
+
+    return y;
+  };
+}
+
+const sum = getSum();
+
+console.log(sum(4));
+console.log(sum(3));
+console.log(sum(10));
+
+// HW 7.2
+
+function multiplyCurrying(a) {
+  return (b) => {
+    return a * b;
+  };
+}
+
+console.log(multiplyCurrying(5)(2));
+
+// HW 7.3
