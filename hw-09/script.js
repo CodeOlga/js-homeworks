cae397af629e692073133d12429975ac3536aa0b
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2024-10-18 15:30:35 +0300

    change some comments

--- hw-09/script.js
+++ hw-09/script.js
@@ -14,19 +14,19 @@ const company = {
 
 function sumSalaries(departments) {
   let total = 0;
-  // перебираемо головний об'єкт company і знаходимо ключ sales
+  // перебираемо головний об'єкт company і знаходимо ключи departments
   for (let key in departments) {
-    const value = departments[key]; // value = sales
+    const value = departments[key]; // value може бути масивом чи об'єктом
 
-    // перевіряємо масивом чи об'єктом внутрішні значення
+    // перевіряємо чи є value масивом
     if (Array.isArray(value)) {
-      // якщо масив - перебираємо элементи масива (два об'єкти(item) по черзі)
+      // якщо масив - перебираємо элементи масива (два об'єкти(employee) по черзі)
       for (let employee of value) {
-        total += employee.salary || 0; // в кожному об'єкті(item) шукаємо ключ salary, якщо він є - додаємо зарплату
+        total += employee.salary || 0; // в кожному об'єкті(employee) шукаємо ключ salary, якщо він є - додаємо зарплату
       }
       // якщо об'єкт та не null (typeof null буде також "object")
     } else if (typeof value === "object" && value !== null) {
-      total += sumSalaries(value); // рекурсивно обробляємо вкладений об'єкт
+      total += sumSalaries(value); // рекурсивно обробляємо вкладені об'єкти (departments)
     }
   }
 
