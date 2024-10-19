7038ab2652be47c6e66b92c54b4fe53dfe9b7708
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2024-10-19 13:28:08 +0300

    add second variant

--- hw-09/script.js
+++ hw-09/script.js
@@ -12,26 +12,41 @@ const company = {
   },
 };
 
-function sumSalaries(departments) {
-  let total = 0;
-  // перебираемо головний об'єкт company і знаходимо ключи departments
-  for (let key in departments) {
-    const value = departments[key]; // value може бути масивом чи об'єктом
+// //===========1 спосіб===============
+// function getTotalSalary(departments) {
+//   let total = 0;
+//   // перебираемо головний об'єкт company і знаходимо ключи departments
+//   for (let key in departments) {
+//     const value = departments[key]; // value може бути масивом чи об'єктом
 
-    // перевіряємо чи є value масивом
-    if (Array.isArray(value)) {
-      // якщо масив - перебираємо элементи масива (два об'єкти(employee) по черзі)
-      for (let employee of value) {
-        total += employee.salary || 0; // в кожному об'єкті(employee) шукаємо ключ salary, якщо він є - додаємо зарплату
-      }
-      // якщо об'єкт та не null (typeof null буде також "object")
-    } else if (typeof value === "object" && value !== null) {
-      total += sumSalaries(value); // рекурсивно обробляємо вкладені об'єкти (departments)
-    }
-  }
+//     // перевіряємо чи є value масивом
+//     if (Array.isArray(value)) {
+//       // якщо масив - перебираємо элементи масива (два об'єкти(employee) по черзі)
+//       for (let employee of value) {
+//         total += employee.salary || 0; // в кожному об'єкті(employee) шукаємо ключ salary, якщо він є - додаємо зарплату
+//       }
+//       // якщо об'єкт та не null (typeof null буде також "object")
+//     } else if (typeof value === "object" && value !== null) {
+//       total += getTotalSalary(value); // рекурсивно обробляємо вкладені об'єкти (departments)
+//     }
+//   }
 
-  return total;
-}
+//   return total;
+// }
 
-const totalSalary = sumSalaries(company);
+// const totalSalary = getTotalSalary(company);
+// console.log(`The total salary of all company employees is ${totalSalary}`);
+
+//===========2 спосіб===============
+function getTotalSalary(departments) {
+  // Перевіряємо, чи є департамент (або піддепартамент) масивом
+  return Array.isArray(departments)
+    ? departments.reduce((totalSum, employee) => totalSum + employee.salary, 0)
+    : Object.values(departments).reduce(
+        (totalSum, subDepartment) => totalSum + getTotalSalary(subDepartment),
+        0
+      );
+} //Object.values приймає об'єкт і повертає масив саме значень, не ключів
+
+const totalSalary = getTotalSalary(company);
 console.log(`The total salary of all company employees is ${totalSalary}`);
