33a377d721defa4b4feaa3757f674bf4317e5406
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2024-11-05 11:25:30 +0200

    leave just one variant

--- hw-13/script.js
+++ hw-13/script.js
@@ -2,28 +2,44 @@
 
 const form = document.querySelector("form");
 const errorMessages = form.querySelectorAll("p");
+const inputs = form.querySelectorAll("input, textarea");
 
 const regExpName = /^[a-zа-я]+$/i; // обов'язкове текстове поле
 const regExpMessage = /.{5,}/; // текстове поле не менше 5 символів
 const regExpPhone = /^\+380\d{9}$/; // обов'язкове поле типу phone. З початком на +380
 const regExpEmail = /^[a-z0-9]+@[a-z]+\.[a-z]{2,}$/i; // email обов'язково повинен мати @ та крапку
 
-const validateField = (regExp, value, errorMessage) => {
+const validateField = (regExp, value, errorMessage, inputElement) => {
   if (!regExp.test(value)) {
     errorMessage.classList.remove("hide");
-
+    inputElement.classList.add("error-border");
     return false; // поле невалідно
   } else {
     errorMessage.classList.add("hide");
-
+    inputElement.classList.remove("error-border");
     return true; // поле валідно
   }
 };
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+// обработка подій focus та blur на inputs
+inputs.forEach((input, index) => {
+  const errorMessage = errorMessages[index];
+  const regExp = [regExpName, regExpMessage, regExpPhone, regExpEmail][index];
 
-  const formData = new FormData(e.target);
+  input.addEventListener("focus", () => {
+    errorMessage.classList.add("hide");
+    input.classList.remove("error-border");
+  });
+
+  input.addEventListener("blur", () => {
+    validateField(regExp, input.value, errorMessage, input);
+  });
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
   const formObj = {};
 
   formData.forEach((value, key) => (formObj[key] = value));
@@ -31,23 +47,39 @@ form.addEventListener("submit", (e) => {
   const { name, message, tel, email } = formObj;
 
   // валідація кожного поля
-  const isNameValid = validateField(regExpName, name, errorMessages[0]);
+  const isNameValid = validateField(
+    regExpName,
+    name,
+    errorMessages[0],
+    inputs[0]
+  );
   const isMessageValid = validateField(
     regExpMessage,
     message,
-    errorMessages[1]
+    errorMessages[1],
+    inputs[1]
   );
-  const isPhoneValid = validateField(regExpPhone, tel, errorMessages[2]);
-  const isEmailValid = validateField(regExpEmail, email, errorMessages[3]);
+  const isPhoneValid = validateField(
+    regExpPhone,
+    tel,
+    errorMessages[2],
+    inputs[2]
+  );
+  const isEmailValid = validateField(
+    regExpEmail,
+    email,
+    errorMessages[3],
+    inputs[3]
+  );
 
   // перевіряємо, що всі поля валідні перед відправкою форми
   if (isNameValid && isMessageValid && isPhoneValid && isEmailValid) {
     console.log(`
-    Name: ${name},
-    Massage: ${message},
-    Phone number: ${tel},
-    Email: ${email}
-  }`);
+      Name: ${name},
+      Message: ${message},
+      Phone number: ${tel},
+      Email: ${email}
+    `);
 
     form.reset();
   }
