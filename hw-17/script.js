9d273e5973061d8315ca0985a6dad8267eb2c1bc
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2024-11-15 14:54:25 +0200

    finished 17.2

--- hw-17/script.js
+++ hw-17/script.js
@@ -1,3 +1,5 @@
+//HW 17.1
+
 // ===========1===========
 // якщо передаємо різні числа
 
@@ -58,3 +60,24 @@ console.log(calc.divide(8, 2)); //4
 // console.log(calc.subtract());
 // console.log(calc.multiply());
 // console.log(calc.divide());
+
+//HW 17.2
+
+class Coach {
+  constructor(name, specialization, rating) {
+    this.name = name;
+    this.specialization = specialization;
+    this.rating = rating;
+  }
+  displayInfo() {
+    console.log(`Coach: ${this.name}, 
+    Specialization: ${this.specialization},
+    Rating: ${this.rating}`);
+  }
+}
+
+const coach1 = new Coach("John Doe", "Fitness", 4.7);
+const coach2 = new Coach("Alice Smith", "Yoga", 4.9);
+
+coach1.displayInfo();
+coach2.displayInfo();
