3b407a132ca86b09ca25fc388f27b7235af7ee40
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2025-01-08 16:12:58 +0200

    add heading

--- hw-30/swapi-app/src/components/Data/Data.jsx
+++ hw-30/swapi-app/src/components/Data/Data.jsx
@@ -1,18 +1,28 @@
 import { useSelector } from "react-redux";
 
-import { swapiItems } from "../../redux/slice/selectors";
+import { swapiItems, apiEndpoint } from "../../redux/slice/selectors";
 
 const Data = () => {
   const data = useSelector(swapiItems);
+  const endpoint = useSelector(apiEndpoint);
+
+  const endpointParts = endpoint.split("/");
+  const endpointInfo =
+    endpointParts.length > 1 ? `${endpointParts[0]} ${endpointParts[1]}` : "";
 
   return (
     <div className="d-flex justify-content-between mb-2 mt-2">
       <div className="border border-secondary rounded p-3 bg-light w-100">
-        {data && typeof data === "object" ? (
-          <pre className="text-secondary">{JSON.stringify(data, null, 2)}</pre>
-        ) : (
-          <p className="text-secondary">No data</p>
-        )}
+        {endpointInfo && <h4>{endpointInfo}</h4>}
+        <div className="border border-secondary rounded p-3 bg-light">
+          {data && typeof data === "object" ? (
+            <pre className="text-secondary">
+              {JSON.stringify(data, null, 2)}
+            </pre>
+          ) : (
+            <p className="text-secondary">No data</p>
+          )}
+        </div>
       </div>
     </div>
   );
